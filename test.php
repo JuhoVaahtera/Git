8ba86765dcbb9d0680a4f5f34a4313b5e70eefa1
LeeviStade <102716637+LeeviStade@users.noreply.github.com>
2023-08-30 14:41:20 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <?php
+require_once 'data-access.php';  
 
-require_once 'data-access.php';
-
-$participant1 = new Participant('Tjimi', 'The God', 'tjimi@example.com');
-$participant2 = new Participant('Tjimitte', 'The Goddes', 'tjmi@example.com');
+// Create participants and events
+$participant1 = new Participant('1', 'Tjimi', 'The God', 'tjimi@example.com');
+$participant2 = new Participant('2', 'Tjimitte', 'The Goddes', 'tjmi@example.com');
 
 $event1 = new Event(
     'Fortinite',
@@ -15,17 +15,19 @@ $event1 = new Event(
 
 $event2 = new Event(
     'Music festivale',
-    'Another fucking Ed Sheeran consert',
+    'Another Ed Sheeran concert',
     '456 Elm Avenue',
     new DateTime('2023-08-24 12:00:00'),
     new DateTime('2023-08-24 18:00:00')
 );
 
-$event1->addParticipant($participant1);
-$event1->addParticipant($participant2);
-$event2->addParticipant($participant1);
-$event2->addParticipant($participant2);
+// Assuming you have a database connection established
+$dbConnection = new mysqli('localhost', 'leevi', 'leevi', 'leevi');
+$dataAccess = new DataAccess($dbConnection);
 
+// Add participants and events using DataAccess methods
+$dataAccess->addParticipant($participant1);
+$dataAccess->addParticipant($participant2);
 ?>
 
 <!DOCTYPE html>
@@ -34,6 +36,7 @@ $event2->addParticipant($participant2);
     <title>Event Information</title>
 </head>
 <body>
+    
     <h1><?php echo $event1->getTitle(); ?></h1>
     <p><strong>Description:</strong> <?php echo $event1->getDescription(); ?></p>
     <p><strong>Address:</strong> <?php echo $event1->getAddress(); ?></p>
@@ -41,23 +44,10 @@ $event2->addParticipant($participant2);
     <p><strong>End Time:</strong> <?php echo $event1->getEndTime()->format('Y-m-d H:i:s'); ?></p>
     <h2>Participants:</h2>
     <ul>
-        <?php foreach ($event1->getParticipants() as $participant): ?>
+        <?php foreach ($dataAccess->getParticipants() as $participant): ?>
             <li><?php echo $participant->getFirstName() . ' ' . $participant->getLastName(); ?> (<?php echo $participant->getEmail(); ?>)</li>
         <?php endforeach; ?>
     </ul>
 
-    <hr>
-
-    <h1><?php echo $event2->getTitle(); ?></h1>
-    <p><strong>Description:</strong> <?php echo $event2->getDescription(); ?></p>
-    <p><strong>Address:</strong> <?php echo $event2->getAddress(); ?></p>
-    <p><strong>Start Time:</strong> <?php echo $event2->getStartTime()->format('Y-m-d H:i:s'); ?></p>
-    <p><strong>End Time:</strong> <?php echo $event2->getEndTime()->format('Y-m-d H:i:s'); ?></p>
-    <h2>Participants:</h2>
-    <ul>
-        <?php foreach ($event2->getParticipants() as $participant): ?>
-            <li><?php echo $participant->getFirstName() . ' ' . $participant->getLastName(); ?> (<?php echo $participant->getEmail(); ?>)</li>
-        <?php endforeach; ?>
-    </ul>
 </body>
 </html>
