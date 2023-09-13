865c8eaf4906d567fdb67da754e943d8d3ea7850
JuhoVaahtera <102716632+JuhoVaahtera@users.noreply.github.com>
2023-09-13 14:04:17 +0300

    s

---
+++ GitNew2/data-access.php
@@ -0,0 +1,309 @@
+<?php
+class DataAccess {
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    // Participant CRUD operations
+    public function addParticipant($participant) {
+        $query = "INSERT INTO participants (first_name, last_name, email, password) VALUES (?,?,?,?)";
+        $stmt = $this->db->prepare($query);
+        $fn = $participant->getFirstName();
+        $ln = $participant->getLastName();
+        $e = $participant->getEmail();
+        $p = $participant->getPassword();
+        $stmt->bind_param('ssss', $fn, $ln, $e, $p);
+        $stmt->execute();
+        $stmt->close();
+    }
+
+    public function getParticipants() {
+        $query = "SELECT * FROM participants";
+        $result = $this->db->query($query);
+
+        $participants = [];
+        while ($row = $result->fetch_assoc()) {
+            $participant = new Participant($row['id'], $row['first_name'], $row['last_name'], $row['email'], $row['password']);
+            $participants[] = $participant;
+        }
+
+        return $participants;
+    }
+
+    public function updateParticipant($participant) {
+        $query = "UPDATE participants SET name = ?, email = ? WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("ssi", $participant->getName(), $participant->getEmail(), $participant->getID());
+        $stmt->execute();
+        $stmt->close();
+    }
+
+    public function deleteParticipant($participant) {
+        $query = "DELETE FROM participants WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("i", $participant->getID());
+        $stmt->execute();
+        $stmt->close();
+    }
+
+    public function getParticipantByEmail($email) {
+        $query = "SELECT * FROM participants WHERE email = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $stmt->close();
+        
+
+        if ($row) {
+            
+            $participant = new Participant($row['id'], $row['first_name'], $row['last_name'], $row['email'], $row['password']);
+            
+            return $participant;
+            
+        }
+    
+        return null;
+    }
+
+
+    public function addEvent($event) { echo  $event->getTitle(). ' ' .count($event->getParticipants());
+        $query = "INSERT INTO events (title, description, address, start_time, end_time) VALUES (?,?,?,?,?)";
+        $stmt = $this->db->prepare($query);
+        $tl = $event->getTitle();
+        $dc = $event->getDescription();
+        $ad = $event->getAddress();
+        $st = $event->getStartTime()->format('Y-m-d H:i:s');
+        $et = $event->getEndTime()->format('Y-m-d H:i:s');
+        $stmt->bind_param('sssss', $tl, $dc, $ad, $st, $et);
+        $stmt->execute();
+        $stmt->close();
+
+        $query = "INSERT INTO events_participants (event_id, participant_id) VALUES (?,?)";
+        $stmt = $this->db->prepare($query);
+        $p = $event->getParticipants();
+        foreach($p as $participant){
+            $stmt->bind_param('dd', $event->getID(), $participant->getID());
+        }
+        $stmt->execute();
+        $stmt->close();
+    }
+    
+    public function getEvents() {
+        $query = "SELECT * FROM events";
+        $result = $this->db->query($query);
+
+        $events = [];
+        while ($row = $result->fetch_assoc()) {
+            $event = new Event($row['id'], $row['title'], $row['description'], $row['address'], $row['start_time'], $row['end_time']);
+            $events[] = $event;
+        }
+
+        return $events;
+    }
+
+    public function updateEvents($event) {
+        $query = "UPDATE events SET name = ?, email = ? WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("ssi", $participant->getName(), $participant->getEmail(), $participant->getID());
+        $stmt->execute();
+        $stmt->close();
+    }
+
+    public function getParticipantById($participantId) {
+        $query = "SELECT * FROM participants WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("i", $participantId);
+        $stmt->execute();
+    
+        $result = $stmt->get_result();
+        
+        if ($result->num_rows === 1) {
+            $row = $result->fetch_assoc();
+            $participant = new Participant($row['id'], $row['first_name'], $row['last_name'], $row['email'], null);
+            return $participant;
+        } else {
+            // Participant not found
+            return null;
+        }
+    }
+
+    public function getEventById($eventId) {
+        $query = "SELECT * FROM events WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("i", $eventId);
+        $stmt->execute();
+    
+        $result = $stmt->get_result();
+        
+        if ($result->num_rows === 1) {
+            $row = $result->fetch_assoc();
+            $event = new Event($row['title'], $row['description'], $row['address'], $row['start_time'], $row['end_time']);
+            return $event;
+        } else {
+            // Event not found
+            return null;
+        }
+    }
+    
+    // Event CRUD operations (similar to Participant operations)
+}
+
+class Participant {
+    private $id;
+    private $firstName;
+    private $lastName;
+    private $email;
+    private $password;
+
+    public function __construct($id, $firstName, $lastName, $email, $password) {
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getID() {
+        return $this->id;
+    }
+
+    public function setID($id) {
+        if ($this->id === null) {
+            $this->id = $id;
+        }
+    }
+
+    public function getFirstName() {
+        return $this->firstName;
+    }
+
+    public function setFirstName($firstName) {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName() {
+        return $this->lastName;
+    }
+
+    public function setLastName($lastName) {
+        $this->lastName = $lastName;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+}
+
+class Event {
+    private $id;
+    private $title;
+    private $description;
+    private $address;
+    private $startTime;
+    private $endTime;
+    private $participants = array();
+    
+    public function __construct($id, $title, $description, $address, $startTime, $endTime) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->address = $address;
+        $this->startTime = $startTime;
+        $this->endTime = $endTime;
+    }
+    public function getID() {
+        return $this->id;
+    }
+    
+    public function getTitle() {
+        return $this->title;
+    }
+    
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+    
+    public function getDescription() {
+        return $this->description;
+    }
+    
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+    
+    public function getAddress() {
+        return $this->address;
+    }
+    
+    public function setAddress($address) {
+        $this->address = $address;
+    }
+    
+    public function getStartTime() {
+        return $this->startTime;
+    }
+    
+    public function setStartTime($startTime) {
+        $this->startTime = $startTime;
+    }
+    
+    public function getEndTime() {
+        return $this->endTime;
+    }
+    
+    public function setEndTime($endTime) {
+        $this->endTime = $endTime;
+    }
+    
+    public function addParticipant($participant) {
+        $this->participants[] = $participant;
+    }
+    
+    public function removeParticipant($participant) {
+        $index = array_search($participant, $this->participants);
+        if ($index !== false) {
+            array_splice($this->participants, $index, 1);
+        }
+    }
+
+    public function getParticipants() {
+        return $this->participants;
+    }
+
+    public function getEvents() {
+        return $this->events;
+    }
+
+       
+}
+// Database connection
+$hostname = "localhost";
+$username = "Juho";
+$password = "juho";
+$database = "juho";
+
+$db = new mysqli($hostname, $username, $password, $database);
+
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+
+$dataAccess = new DataAccess($db);
+// Voit nyt käyttää $dataAccessia osallistujien ja tapahtumien tietojen käsittelyyn.
+?>
---
+++ GitNew2/event_form.php
@@ -0,0 +1,102 @@
+<?php
+require_once 'data-access.php';
+
+// Assuming you have a database connection established
+$dbConnection = new mysqli('localhost', 'Juho', 'juho', 'juho');
+$dataAccess = new DataAccess($dbConnection);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Check if the required form fields are set
+    if (isset($_POST['title'], $_POST['description'], $_POST['address'], $_POST['startTime'], $_POST['endTime'], $_POST['participants'])) {
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $address = $_POST['address'];
+        $startTime = $_POST['startTime'];
+        $endTime = $_POST['endTime'];
+        $startTime = new DateTime($startTime);
+        $endTime = new DateTime($endTime);
+
+        // Create a new Event object with the form data
+        $newEvent = new Event(null, $title, $description, $address, $startTime, $endTime);
+
+        // Add participants based on selected checkboxes
+        if (isset($_POST['participants']) && is_array($_POST['participants'])) {
+            foreach ($_POST['participants'] as $participantId) {
+                // Fetch the participant from the database based on the ID
+                $participant = new Participant($participantId, null, null, null, null);
+                if ($participant) {
+                    $newEvent->addParticipant($participant);
+                }
+            }
+        }
+        echo  $newEvent->getTitle(). ' d ' .count($newEvent->getParticipants());
+        // Now, use the DataAccess class to add the new event to the database
+        if ($dataAccess->addEvent($newEvent)) {
+            // Event and participants added successfully
+            echo "Event and participants added successfully.";
+        } else {
+            // Error handling for insertion failure
+            echo "Error adding event and participants.";
+        }
+
+        // You can also add redirection logic here if needed
+    } else {
+        // Handle case when required form fields are missing or contain invalid data
+        echo "Please fill out all required fields with valid data.";
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Event Form</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8">
+</head>
+<body>
+    <nav>
+        <a href="test.php">Home</a>
+        <a href="kayttajat.php">Käyttäjät</a>
+        <a href="event_form.html">Lisää eventti</a>
+        <a href="login.html">kirjaudu</a>
+    </nav>
+    <h2>Add Event</h2>
+
+    <form action="event_form.php" method="post">
+        <label for="title">Title:</label>
+        <input type="text" name="title" required><br>
+        
+        <label for="description">Description:</label>
+        <textarea name="description" required></textarea><br>
+        
+        <label for="address">Address:</label>
+        <input type="text" name="address" required><br>
+        
+        <label for="startTime">Start Time:</label>
+        <input type="datetime-local" name="startTime" required><br>
+        
+        <label for="endTime">End Time:</label>
+        <input type="datetime-local" name="endTime" required><br>
+
+        <h3>Select Participants:</h3>
+
+        <?php
+            
+            $dbConnection = new mysqli('localhost', 'Juho', 'juho', 'juho');
+            $dataAccess = new DataAccess($dbConnection);
+
+            $participants = $dataAccess->getParticipants();
+
+            foreach ($participants as $participant) {
+                echo '<input type="checkbox" name="participants[]" value="' . $participant->getId() . '">';
+                echo $participant->getFirstName() . ' ' . $participant->getLastName() . '<br>';
+            }
+        ?>
+        <p></p>
+        <input type="submit" value="Add Event">
+    </form>
+</body>
+</html>
